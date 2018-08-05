36f60602ccfc39b2b72f20c163e0c5046fb81a16
yifish <35884324+yifish@users.noreply.github.com>
2018-08-05 16:32:00 +0800

    后台控制器

---
+++ app/Http/Controllers/admin/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/5
+ * Time: 16:20
+ */
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function login()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/admin/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/5
+ * Time: 16:19
+ */
+
+namespace App\Http\Controllers\admin;
+
+class LoginController extends AdminController
+{
+
+}
