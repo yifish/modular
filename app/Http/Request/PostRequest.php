69d7d23cdc5766b3de7bb51659777122ac6930ba
yifish <35884324+yifish@users.noreply.github.com>
2018-08-05 17:06:24 +0800

    验证机制

---
+++ app/Http/Request/PostRequest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/5
+ * Time: 17:04
+ */
+
+namespace App\Http\Request;
+
+use Illuminate\Http\Request;
+
+class PostRequest extends Request
+{
+    
+}
