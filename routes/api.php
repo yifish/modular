f6457411978c9a732ff2373c77d03f71e3f19235
yifish <35884324+yifish@users.noreply.github.com>
2018-08-05 16:19:08 +0800

    路由

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,10 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Api\Controllers\admin'], function ($api) {
+        $api->get('login', 'LoginController@login');
+    });
 });
