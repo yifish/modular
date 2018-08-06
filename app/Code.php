fac9d34676a3b2505c302bdb6796a6c317c6fc84
yifish <35884324+yifish@users.noreply.github.com>
2018-08-06 22:33:30 +0800

    验证密钥

---
+++ app/Code.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/6
+ * Time: 22:29
+ */
+
+namespace App;
+
+class Code
+{
+    const SUCCESS           = 200; // 成功
+
+}
---
+++ app/MyCommon/Token.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/6
+ * Time: 22:08
+ */
+
+namespace App\MyCommon;
+
+/**
+ * Token类
+ */
+class Token extends CipherCommon
+{
+    public function __construct($ranDomCodeNumber = 32)
+    {
+        $this->ranDomCode = $this->getString();
+        $this->ranDomCodeNumber = $ranDomCodeNumber;
+    }
+    /**
+     * 自定义token加密
+     * @param $string string token字符串
+     * @return string 返回字符串
+     */
+    public function rule($string)
+    {
+        return $string;
+    }
+    /**
+     * 加密token
+     * @return string 返回字符串
+     */
+    public function encryption()
+    {
+        $this->ranDomCode = $this->setMD5($this->ranDomCode);
+        return bcrypt($this->rule($this->ranDomCode));
+    }
+
+
+
+}
