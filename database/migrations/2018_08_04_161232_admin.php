241d64e3452c4814cb914816eb12b601bb7edffc
yifish <35884324+yifish@users.noreply.github.com>
2018-10-04 23:57:54 +0800

    后台账号信息图片

--- database/migrations/2018_08_04_161232_admin.php
+++ database/migrations/2018_08_04_161232_admin.php
@@ -19,6 +19,7 @@ class Admin extends Migration
             $table->string('name', 50)->comment('管理员名称');
             $table->string('loginName', 20)->unique()->comment('登陆账号');
             $table->string('password', 250)->comment('密码');
+            $table->string('images', 300)->default('')->nullable()->comment('图片地址');
             $table->string('random', 6)->comment('随机码');
             $table->string('token', 64)->nullable()->comment('验证码');
             $table->string('loginTime',13)->default(0)->comment('最后一次登陆时间');
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -132,7 +132,13 @@
 
             <li class="am-dropdown" data-am-dropdown data-am-dropdown-toggle>
                 <a class="am-dropdown-toggle tpl-header-list-link" href="javascript:;">
-                    <span class="tpl-header-list-user-nick">禁言小张</span><span class="tpl-header-list-user-ico"> <img src="{{asset('assets/img/user01.png')}}"></span>
+                    <span class="tpl-header-list-user-nick">{{ session('admin.name') }}</span><span class="tpl-header-list-user-ico">
+                        @if (empty(session('admin.images')))
+                            <img src="{{asset('images/admin_portrait.gif')}}">
+                        @else
+                            <img src="{{ session('admin.images') }}">
+                        @endif
+                    </span>
                 </a>
                 <ul class="am-dropdown-content">
                     {{--<li><a href="#"><span class="am-icon-bell-o"></span> 资料</a></li>--}}
