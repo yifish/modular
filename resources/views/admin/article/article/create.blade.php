dfeba9716a03f6e73def221cf76fc2de3d7cf2cd
yifish <35884324+yifish@users.noreply.github.com>
2018-10-21 00:05:02 +0800

    文章内容编辑

--- resources/views/admin/article/article/create.blade.php
+++ resources/views/admin/article/article/create.blade.php
@@ -1,6 +1,15 @@
 @extends('admin.layouts.app')
 
 @section('content')
+    <style>
+        .w-e-menu{
+            display: inline-block;
+        }
+        .w-e-toolbar{
+            display: inline-block;
+            width: 100%;
+        }
+    </style>
     <div class="tpl-content-wrapper">
         <div class="tpl-content-page-title">
 
@@ -35,12 +44,12 @@
                             @endif
 
                             <div class="am-form-group">
-                                <label for="admin-name" class="am-u-sm-3 am-form-label">标题</label>
+                                <label for="article-name" class="am-u-sm-3 am-form-label">标题</label>
                                 <div class="am-u-sm-9">
                                     @if($formType == 'update')
-                                        <input type="text" id="admin-name" name="title" value="{{$article->title}}" placeholder="请输入标题">
+                                        <input type="text" id="article-name" name="title" value="{{$article->title}}" placeholder="请输入标题">
                                     @else
-                                        <input type="text" id="admin-name" name="title" value="{{old('title')}}" placeholder="请输入标题">
+                                        <input type="text" id="article-name" name="title" value="{{old('title')}}" placeholder="请输入标题">
                                     @endif
                                     @if ($errors->has('title'))
                                         <small style="color:red;">{{$errors->first('title')}}</small>
@@ -49,12 +58,12 @@
                             </div>
 
                             <div class="am-form-group">
-                                <label for="admin-name" class="am-u-sm-3 am-form-label">简介</label>
+                                <label for="article-name" class="am-u-sm-3 am-form-label">简介</label>
                                 <div class="am-u-sm-9">
                                     @if($formType == 'update')
-                                        <input type="text" id="admin-name" name="intro" value="{{$article->intro}}" placeholder="请输入简介">
+                                        <input type="text" id="article-name" name="intro" value="{{$article->intro}}" placeholder="请输入简介">
                                     @else
-                                        <input type="text" id="admin-name" name="intro" value="{{old('intro')}}" placeholder="请输入简介">
+                                        <input type="text" id="article-name" name="intro" value="{{old('intro')}}" placeholder="请输入简介">
                                     @endif
                                     @if ($errors->has('intro'))
                                         <small style="color:red;">{{$errors->first('intro')}}</small>
@@ -63,9 +72,9 @@
                             </div>
 
                             <div class="am-form-group">
-                                <label for="admin-roleId" class="am-u-sm-3 am-form-label">分类</label>
+                                <label for="article-roleId" class="am-u-sm-3 am-form-label">分类</label>
                                 <div class="am-u-sm-9">
-                                    <select id="admin-roleId" name="classId">
+                                    <select id="article-roleId" name="classId">
                                         <option value="-1">请选择分类</option>
                                         @foreach($articleClass as $key => $value)
                                             @if ($formType == 'update' && $article->classId == $value->id)
@@ -73,7 +82,6 @@
                                             @else
                                                 <option value="{{ $value->id }}">{{ $value->name }}</option>
                                             @endif
-
                                         @endforeach
                                     </select>
                                     @if ($errors->has('classId'))
@@ -82,7 +90,29 @@
                                 </div>
                             </div>
 
+                            <div class="am-form-group">
+                                <label for="article-thumbnail" class="am-u-sm-3 am-form-label">封面图片</label>
+                                <div class="am-u-sm-9">
+                                    <div class="am-form-group am-form-file">
+                                        <div class="tpl-form-file-img" style="width: 100px;">
+                                            @if ($article->thumbnail)
+                                                <img for-src="article-thumbnail" src="{{$article->thumbnail}}" alt="">
+                                            @else
+                                                <img for-src="article-thumbnail" src="{{asset('/images/no-images.png')}}" alt="">
+                                            @endif
+                                        </div>
+                                        <button type="button" class="am-btn am-btn-danger am-btn-sm"><i class="am-icon-cloud-upload"></i>添加封面图片</button>
+                                        <input id="article-thumbnail" type="file" name="thumbnail" multiple>
+                                    </div>
+                                </div>
+                            </div>
 
+                            <div class="am-form-group">
+                                <label for="article-content" class="am-u-sm-3 am-form-label">内容</label>
+                                <div class="am-u-sm-9">
+                                    <div id="article-content"></div>
+                                </div>
+                            </div>
 
                             <div class="am-form-group">
                                 <div class="am-u-sm-9 am-u-sm-push-3">
@@ -97,4 +127,46 @@
             <div class="tpl-alert"></div>
         </div>
 
+@endsection
+@section('script')
+    <script src="{{asset('/js/wangEditor.min.js')}}"></script>
+    <script>
+        function getPhoto(node) {
+            var imgURL = "";
+            try{
+                var file = null;
+                if(node.files && node.files[0] ){
+                    file = node.files[0];
+                }else if(node.files && node.files.item(0)) {
+                    file = node.files.item(0);
+                }
+                //Firefox 因安全性问题已无法直接通过input[file].value 获取完整的文件路径
+                try{
+                    imgURL =  file.getAsDataURL();
+                }catch(e){
+                    imgRUL = window.URL.createObjectURL(file);
+                }
+            }catch(e){
+                if (node.files && node.files[0]) {
+                    var reader = new FileReader();
+                    reader.onload = function (e) {
+                        imgURL = e.target.result;
+                    };
+                    reader.readAsDataURL(node.files[0]);
+                }
+            }
+            return imgRUL;
+        }
+
+        $(function(){
+            $("#admin-thumbnail").on('change', function(){
+                var imgRUL = getPhoto(this);
+                $("img[for-src='"+ $(this).attr('id') +"']").attr('src', imgRUL);
+            })
+            var E = window.wangEditor
+            var editor = new E('#article-content')
+            editor.customConfig.uploadImgShowBase64 = true   // 使用 base64 保存图片
+            editor.create()
+        })
+    </script>
 @endsection
