9b9b3c310f93b787d4e18c765ab632adab5ea74a
yifish <35884324+yifish@users.noreply.github.com>
2018-12-08 16:49:13 +0800

    轮播图

---
+++ app/Http/Controllers/AdminWeb/Spread/Banner.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/8
+ * Time: 16:45
+ */
+
+namespace App\Http\Controllers\AdminWeb\Spread;
+
+use App\Http\Controllers\AdminWeb\AdminWebController;
+use Illuminate\Http\Request;
+
+class Banner extends AdminWebController
+{
+    private $banner;
+
+    public function __construct()
+    {
+        
+    }
+
+    /*
+     * 轮播图页
+     * */
+    public function barnerList()
+    {
+
+    }
+}
